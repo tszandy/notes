70dfa43252ade1efabfd39ee7c178250f06721e8
Wei Xie <444980834@qq.com>
2022-01-19 21:07:34 -0800

    update notes

---
+++ cpp/cpp_data_note.sh
@@ -0,0 +1,32 @@
+https://en.cppreference.com/w/cpp/language/types#char8_t
+
+# 1 byte
+bool , char , char8_t , unsigned char , signed char , __int8	
+# 2 bytes
+char16_t , __int16 , short , unsigned short , wchar_t , __wchar_t	
+# 4 bytes
+char32_t , float , __int32 , int , unsigned int , long , unsigned long	
+# 8 bytes
+double , __int64 , long double , long long , unsigned long long	
+
+# bool
+    1 byte
+
+# char 
+    1 byte
+    ranges from 0 to 255
+
+# Int
+    4 bytes
+    ranges from -2147483648 to 2147483647. 
+
+# size_t the unsigned integer
+    8 bytes
+    ranges from 0 to 18446744073709551615
+
+# string
+    32 bytes
+
+# double
+    8 bytes
+
--- cpp/cpp_note.sh
+++ cpp/cpp_note.sh
@@ -11,17 +11,6 @@ to_string
 #include <vector>
 vector
 
-# 1 byte
-bool , char , char8_t , unsigned char , signed char , __int8	
-# 2 bytes
-char16_t , __int16 , short , unsigned short , wchar_t , __wchar_t	
-# 4 bytes
-char32_t , float , __int32 , int , unsigned int , long , unsigned long	
-# 8 bytes
-double , __int64 , long double , long long , unsigned long long	
-
-
-size_t # the unsigned integer 8 bytes 
 
 Eigen::Matrix<double,-1,-1>
 
