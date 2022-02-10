efacb689c00ae491b22fe025398c01b1324db9d3
Wei Xie <444980834@qq.com>
2022-02-10 13:27:57 -0800

    update notes

--- angular_note.sh
+++ angular_note.sh
@@ -5,3 +5,27 @@ npm install --global @angular/cli
 
 ng new newproject --routing
 ng add @angular/material
+
+MVC architectural - model-view-controller
+Model is the data presented to the user.
+View is the user interface.
+Controller combines both. it applies the model/data on a template/view.
+
+MVVM architectural - model-view-viewmodel
+
+Angular is framework for building front-end applications. It is an open source project driven by google.
+
+The 1.x version are referred to as AngularJS
+Everything beyond version 2.0 is just Angular.
+8.2.7
+major_version.minor_version.patch_level
+8 is major_version
+2 is minor_version
+7 is patch_level
+
+TypeScript is a superset of ECMAScript. It is an open source project backed by Microsoft.
+
+Material Design is Google's design language
+
+Design Language is developed to bring consistency in the look and feel.
+
