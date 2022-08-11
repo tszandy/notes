1545be4db996dbbdb4177ad97aa4b32c8552c4cc
wei <444980834@qq.com>
2022-08-11 15:58:50 -0400

    add windows notes

--- windows_note.sh
+++ windows_note.sh
@@ -17,3 +17,14 @@ win + tab then delete
 selecting View > Details, then selecting Add Columns > ensuring Date Modified is checked, 
 and then Sort by > Date Modified > check Decending ... then you go to Folder Options, and View Tab 
 and select the Apply to Folders
+
+# delete services
+sc delete "$service_name"
+
+# register dll file
+regsvr32 ${path_to_dll_file}
+
+# unregister dll file
+regsvr32 /u /s ${path_to_dll_file}
+/u    unregister server calling dll unregister server.
+/s    silent; display no message boxes.
