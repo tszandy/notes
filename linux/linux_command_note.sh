afcc9adea29796db598832a82e1afb3498b7af86
Wei Xie <444980834@qq.com>
2023-03-26 23:34:38 -0400

    update_notes

--- linux/linux_command_note.sh
+++ linux/linux_command_note.sh
@@ -751,3 +751,36 @@ git
 restore unstaged file 
 git clean -df *
 
+# ssh-keygen
+ssh-keygen [-q] [-b bits] [-C comment] [-f output_keyfile] [-m format]
+            [-N new_passphrase] [-t dsa | ecdsa | ed25519 | rsa]
+ssh-keygen -p [-f keyfile] [-m format] [-N new_passphrase]
+            [-P old_passphrase]
+ssh-keygen -i [-f input_keyfile] [-m key_format]
+ssh-keygen -e [-f input_keyfile] [-m key_format]
+ssh-keygen -y [-f input_keyfile]
+ssh-keygen -c [-C comment] [-f keyfile] [-P passphrase]
+ssh-keygen -l [-v] [-E fingerprint_hash] [-f input_keyfile]
+ssh-keygen -B [-f input_keyfile]
+ssh-keygen -D pkcs11
+ssh-keygen -F hostname [-lv] [-f known_hosts_file]
+ssh-keygen -H [-f known_hosts_file]
+ssh-keygen -R hostname [-f known_hosts_file]
+ssh-keygen -r hostname [-g] [-f input_keyfile]
+ssh-keygen -G output_file [-v] [-b bits] [-M memory] [-S start_point]
+ssh-keygen -f input_file -T output_file [-v] [-a rounds] [-J num_lines]
+            [-j start_line] [-K checkpt] [-W generator]
+ssh-keygen -I certificate_identity -s ca_key [-hU] [-D pkcs11_provider]
+            [-n principals] [-O option] [-V validity_interval]
+            [-z serial_number] file ...
+ssh-keygen -L [-f input_keyfile]
+ssh-keygen -A [-f prefix_path]
+ssh-keygen -k -f krl_file [-u] [-s ca_public] [-z version_number]
+            file ...
+ssh-keygen -Q -f krl_file file ...
+ssh-keygen -Y check-novalidate -n namespace -s signature_file
+ssh-keygen -Y sign -f key_file -n namespace file ...
+ssh-keygen -Y verify -f allowed_signers_file -I signer_identity
+        -n namespace -s signature_file [-r revocation_file]
+example:
+ssh-keygen -b 2048 -t rsa
