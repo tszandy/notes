21d348bbd44985ccfd72c206495c02a747fa9c04
tszandy <xwddn1994@gmail.com>
2023-06-05 11:59:36 -0400

    Create github_desktop_notes.sh

---
+++ github_desktop_notes.sh
@@ -0,0 +1,44 @@
+# repository list
+ctrl + t 
+
+# branch list
+ctrl + b
+
+# open repository in visual studio code
+ctrl + shift + a 
+
+# open repository in explorer
+ctrl + shift + f
+
+# open repository in github
+ctrl + shift + g
+
+# open repository in command Prompt
+ctrl + `
+
+# open repository from url
+ctrl + o
+
+# clone repository from url
+ctrl + shift + o
+
+# change list
+ctrl + 1
+
+# History
+ctrl + 2
+
+# git push
+ctrl + p
+
+# git pull
+ctrl + shift + p
+
+# git fetch 
+ctrl + shift + T
+
+# create issue
+ctrl + i
+
+# new branch
+ctrl + shift + n
