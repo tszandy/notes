fa5cc9adbdf9767632d66c2c8ffb34aebfb1575f
tszandy <xwddn1994@gmail.com>
2024-05-22 22:20:06 -0400

    update_notes

--- vscode/vscode_extension_notes.sh
+++ vscode/vscode_extension_notes.sh
@@ -23,3 +23,14 @@ Playwright Test for VSCode
 Git Temporal
 Git Graph
 Gitlens
+Rainbow CSV
+Paste JSON as Code
+Path Autocomplete
+Path IntelliSense
+Auto Rename Tag
+Better Align
+Paste and Indent
+Surround
+Wrap Selection
+Code Runner # Run code snippet or code file for multiple languages: C, C++, Java, JavaScript, PHP, Python, Perl, Ruby, Go, Lua, Groovy, PowerShell, BAT/CMD, BASH/SH, F# Script, C# Script, VBScript, TypeScript, CoffeeScript, Scala, Swift, Julia, Crystal, OCaml Script
+JFrog # Adds JFrog Xray scanning of project dependencies to your VS Code IDE. It allows developers to view panels displaying vulnerability information about the components and their dependencies directly in their VS Code IDE. The extension also allows developers to track the status of the code while it is being built, tested and scanned on the CI server.
