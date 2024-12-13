516554d8f3ac392fb760f2e1149f699cecbf57c2
tszandy <444980834@qq.com>
2024-12-13 15:21:40 -0500

    update_notes

--- OS_install_list/mac_OS_install_list_note.sh
+++ OS_install_list/mac_OS_install_list_note.sh
@@ -17,3 +17,6 @@ Texpad
 VMware Fusion Player Personal Use License # Fusion Player offers a Personal Use License, available for free with a valid CustomerConnect account. Home users, Open Source contributors, students, and anyone else can use Fusion Player Free for Non-Commercial activity.
 
 Duplicate File Finder # Find and remove duplicate files
+
+brew install task-spooler # A scheduler for GPU/CPU tasks
+
---
+++ task-spooler_note.sh
@@ -0,0 +1,9 @@
+brew install task-spooler
+
+ts commands
+
+# task spooler list job
+ts -l
+
+# task spooler help
+ts -h
