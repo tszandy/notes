60abc9cac9dc80fb29ee09e282fd9ea6d4a180bd
tszandy <xwddn1994@gmail.com>
2025-05-21 18:14:08 -0400

    update_notes

--- enterprise_llm_model_notes.sh
+++ enterprise_llm_model_notes.sh
@@ -1,8 +1,20 @@
-cursor # https://cursor.com/
-grok # https://grok.com/
+Abridge
 chatgpt # https://chatgpt.com/
 claude # https://claude.ai/login?returnTo=%2F%3F#pricing
-gemini # https://gemini.google.com/app
-smithery # https://smithery.ai/
-perplexity # https://perplexity.ai/?login-source=oneTapHome&login-new=false
+codex
+cursor # https://cursor.com/
 deepseek # https://deepseek.com/
+Devon
+Gemini # https://gemini.google.com/app
+grok # https://grok.com/
+Harvey
+jules
+Manis
+Notebook LM
+Otter
+perplexity # https://perplexity.ai/?login-source=oneTapHome&login-new=false
+Reflect
+Repet (also referred to as Replit)
+Sesame
+smithery # https://smithery.ai/
+Windsurf
