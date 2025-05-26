ed70817ba0fc150e50797804b5deff6405d3eccb
tszandy <xwddn1994@gmail.com>
2025-05-26 12:42:07 -0400

    update_notes

--- ideas/idea_notes.sh
+++ ideas/idea_notes.sh
@@ -1,1 +1,6 @@
-How to build a software/dependency that is easily seperable and scalable. 
+How to build a software/dependency that is easily seperable and scalable.
+
+How to build a system that can easily sync photo.
+    1. requirements
+        1.1. unique and easy to remember name
+        1.2. easy to use on both local and remote
